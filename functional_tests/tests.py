867fbb25039268c0d9f14f178a799f94ab1918ec
angeldrop <fffdan111@163.com>
2020-05-29 11:31:36 +0800

    优化功能测试功能

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
 
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪丝听说有一个很酷的在线待办事项应用
         #她去看了这个应用的首页
-        self.browser.get('http://localhost:8001')
+        self.browser.get(self.live_server_url)
 
         #她注意到网页的标题和头部都包含“待办事项”这个词
         self.assertIn('待办事项系统' ,self.browser.title)
@@ -62,6 +63,4 @@ class NewVisitorTest(unittest.TestCase):
 
         #她很满意，去睡觉了
         
-        
-if __name__=="__main__":
-    unittest.main()
+ 
