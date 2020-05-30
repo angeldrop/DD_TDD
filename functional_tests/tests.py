5a6d55be005ee977bfde20d0634feaf8ddba99aa
angeldrop <fffdan111@163.com>
2020-05-30 23:44:56 +0800

    增加功能测试对多清单，唯一URL的测试。

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,21 +1,34 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
+MAX_WAIT=10
 class NewVisitorTest(LiveServerTestCase):
+
+
     def setUp(self):
         self.browser=webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text,[row.text for row in rows])
 
+    def wait_for_row_in_list_table(self,row_text):
+        start_time=time.time()
+        while True:
+            try:
+                table=self.browser.find_element_by_id('id_list_table')
+                rows=table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError,WebDriverException) as e:
+                if time.time()-start_time>MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪丝听说有一个很酷的在线待办事项应用
         #她去看了这个应用的首页
@@ -25,7 +38,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('待办事项系统' ,self.browser.title)
         head_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('你的清单' ,head_text)
-        
+
         #应用邀请她输入一个待办事项
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -40,27 +53,71 @@ class NewVisitorTest(LiveServerTestCase):
         #她按回车键后，页面更新了
         #待办事项表格中显示了“1、购买孔雀羽毛”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1:购买孔雀羽毛')
-        
+        self.wait_for_row_in_list_table('1:购买孔雀羽毛')
+
         #页面中又显示了一个文本框，可以输入其他的待办事项
         #她输入了“使用孔雀羽毛做假蝇”
         #伊迪丝做事很有条理
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('使用孔雀羽毛做假蝇')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
+
+
         #页面再次更新，她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1:购买孔雀羽毛')
-        self.check_for_row_in_list_table('2:使用孔雀羽毛做假蝇')
+        self.wait_for_row_in_list_table('1:购买孔雀羽毛')
+        self.wait_for_row_in_list_table('2:使用孔雀羽毛做假蝇')
+        self.browser.quit()
         
+        
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #伊迪丝新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('购买孔雀羽毛')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:购买孔雀羽毛')
+
         #伊迪丝想知道这个网站是否会记住她的清单
         #她看到网站为她生成了一个唯一的URL
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+        
+        #现在一名叫作弗朗西斯的新用户访问了网站
+        
+        ##我们使用一个新浏览器会话
+        ##确保伊迪丝的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser=webdriver.Firefox()
+        
+        #弗朗西斯访问首页
+        #页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('购买孔雀羽毛',page_text)
+        self.assertNotIn('使用孔雀羽毛做假蝇',page_text)
+        
+        #弗朗西斯输入一个新待办事项，新建一个清单
+        #他不像伊迪丝那样兴趣盎然
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('买牛奶')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:买牛奶')
+        
+        #弗朗西斯获得了他的唯一URL
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+        
+        #这个页面还是没有伊迪丝的清单
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('购买孔雀羽毛',page_text)
+        self.assertNotIn('使用孔雀羽毛做假蝇',page_text)
+        
+        #两人都很满意，然后去睡觉了
         #而且页面中有一些文字解说这个功能
         self.fail('结束测试')
         #她访问那个URL，发现她的待办事项列表还在
 
         #她很满意，去睡觉了
-        
+
  
