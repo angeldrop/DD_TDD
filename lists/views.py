df210c64bfabaa04f22696702f1fec8048902c87
angeldrop <fffdan111@163.com>
2020-05-31 15:04:43 +0800

    增加了新增到已有列表视图、URL，

--- lists/views.py
+++ lists/views.py
@@ -7,19 +7,26 @@ def home_page(request):
        return render(request,'lists/home.html')
 
 
-def view_list(request):
+def view_list(request,list_id):
     if request.method=='POST':
         list_=List.objects.create()
         Item.objects.create(text=request.POST['item_text'],list=list_)
-        return redirect('/lists/the-only-list-in-the-world/')
+        return redirect(f'/lists/{list_id}/')
  
-    items=Item.objects.all()
+    
+    list_=List.objects.get(id=list_id)
+    items=Item.objects.filter(list=list_)
     return render(request,'lists/list.html',{
-        'items':items
+        'list':list_
     })
 
 
 def new_list(request):
     list_=List.objects.create()
     Item.objects.create(text=request.POST['item_text'],list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request,list_id):
+    list_=List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'],list=list_)
+    return redirect(f'/lists/{list_.id}/')
